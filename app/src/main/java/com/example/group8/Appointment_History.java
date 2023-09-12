dbf0fd140ed80fd61bc6cc7a320b6db2a9e55b24
Christer Miroy <chrstrzm.mry@gmail.com>
2023-09-13 04:45:15 +0800

    Layouts Done

---
+++ app/src/main/java/com/example/group8/Appointment_History.java
@@ -0,0 +1,14 @@
+package com.example.group8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Appointment_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_history);
+    }
+}
