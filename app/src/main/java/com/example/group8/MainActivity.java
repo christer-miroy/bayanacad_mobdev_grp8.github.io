4661250a69dfe95277a2ddc88d08d45ff65a111d
rqd <conchadaregan221@gmail.com>
2023-09-14 20:10:31 +0800

    History recycler view item ui

--- app/src/main/java/com/example/group8/MainActivity.java
+++ app/src/main/java/com/example/group8/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.group8;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView btn_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_history = (CardView) findViewById(R.id.viewAppointment);
+
+        btn_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Appointment_History.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
